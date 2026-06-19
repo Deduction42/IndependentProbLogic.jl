4c59ce7eae6169895fb99c0b58d9ddd291cd4261
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2026-06-19 12:35:24 -0600

    Updated functionality, removed GaussianCDF (gaussian diagnosis is actually logistic)

--- src/logic.jl
+++ src/logic.jl
@@ -1,15 +1,69 @@
 
 Base.getindex(l::AbstractLikelihood) = l.value
-Base.:+(L1::T, LN::T...) where T <: AbstractLikelihood = T(+(map(getindex, (L1, LN...))...))
+Base.:+(L1::T, LN::T...) where T <: AbstractLikelihood = T(+(map(getindex, promote(L1, LN...))...))
 Base.:-(L1::T) where T <: AbstractLikelihood = T(-L1[])
-Base.:-(L1::T, L2::T) where T <: AbstractLikelihood = T(L1[] - L2[])
-Base.:*(L1::T, LN::T...) where T <: AbstractLikelihood = T(*(map(getindex, (L1, LN...))...))
-Base.:/(L1::T, L2::T) where T <: AbstractLikelihood = T(L1[] / L2[])
-
+Base.:-(L1::T, L2::T) where T <: AbstractLikelihood = T(-(map(getindex, promote(L1, L2))...))
+Base.:*(L1::T, LN::T...) where T <: AbstractLikelihood = T(*(map(getindex, promote(L1, LN...))...))
+Base.:/(L1::T, L2::T) where T <: AbstractLikelihood = T(/(map(getindex, promote(L1, L2))...))
 
 fillnan(x::T, y) where T = ifelse(isnan(x), convert(T, y), x)
-Base.isnan(L::LogLik) = isnan(L[])
+nan2zero(x::T) where T = ifelse(isnan(x), zero(x), x)
+nan2one(x::T) where T = ifelse(isnan(x), one(x), x)
+Base.isnan(lik::AbstractLikelihood) = isnan(lik[])
+Base.zero(lik::L) where L <: AbstractLikelihood = L(0)
 
-Base.:!(L::LogLik) = log1mexp(L[])
-Base.:&(L1::LogLik, L2::LogLik) = fillnan(L1 + L2, fillnan(L1, L2)) #Ignore NaNs when combining
-Base.:|(L1::LogLik, L2::LogLik) = !(!L1 & !L2)
+Base.:≈(lik1::AbstractLikelihood, lik2::AbstractLikelihood) = ≈(promote(lik1, lik2)...)
+Base.:≈(lik1::LogLik, lik2::LogLik) = (lik1.value ≈ lik2.value)
+Base.:≈(lik1::Lik, lik2::Lik) = (lik1.value ≈ lik2.value)
+
+
+"""
+    !(lik::AbstractLikelihood)
+
+Complement of the probability returns `(1-lik)` for likelihoods (or computes log1mexp(lik) for log-likelihoods)
+"""
+Base.:!(lik::LogLik) = LogLik(log1mexp(lik[]))
+Base.:!(lik::Lik) = Lik(1-lik[])
+
+"""
+    ∧(lik::AbstractLikelihood, liks::AbstractLikelihood...)
+
+Independent probabilistic `and` function, multiplies likelihoods (or adds log-likelihoods)
+"""
+^(lik::AbstractLikelihood, liks::AbstractLikelihood) = ∧(promote(lik,liks...)...)
+∧(lik::Lik, liks::Lik...) = mapreduce(nan2one, *, (lik, liks...))
+∧(lik::LogLik, liks::LogLik...) = mapreduce(nan2zero, +, (lik, liks...))
+
+"""
+    ⟇(lik::AbstractLikelihood, liks::AbstractLikelihood...)
+
+Independent probabilistic `or` function, multiplies complement likelihoods (or adds complement log-likelihoods)
+and returns the complement of the result
+"""
+∨(lik::AbstractLikelihood, liks::AbstractLikelihood) = ∨(promote(lik,liks...)...)
+∨(lik::Lik, liks::Lik...) = !prod(x->nan2one(!x), (lik, liks...))
+∨(lik::LogLik, liks::LogLik...) = !sum(x->nan2zero(!x), (lik, liks...))
+
+
+singleweight(lik::Lik, n) = Lik(lik.value^(1/n))
+singleweight(lik::LogLik, n) = LogLik(lik.value/n)
+
+"""
+    ⟑(lik::AbstractLikelihood, liks::AbstractLikelihood...)
+
+Pseudo-independent probabilistic `and` function. Returns a reduced-weight comibination (geometric mean for likelihoods
+linear mean for log-likelihoods). Useful for pseudo-independent observations, combining multiple observations with the weight of one.
+"""
+⟑(lik::AbstractLikelihood, liks::AbstractLikelihood...) = singleweight(∧(lik, liks...), count(!isnan, (lik, liks...)))
+
+"""
+    ⟇(lik::AbstractLikelihood, liks::AbstractLikelihood...)
+
+Pseudo-independent probabilistic `or` function. Returns a reduced-weight comibination (geometric mean for likelihoods
+linear mean for log-likelihoods). Useful for pseudo-independent observations, combining multiple observations with the weight of one.
+"""
+⟇(lik::AbstractLikelihood, liks::AbstractLikelihood...) = singleweight(∨(lik, liks...), count(!isnan, (lik, liks...)))
+
+#Logg-ods ratio
+logit(lik::Lik) = logit(lik[])
+logit(lik::LogLik) = lik[] - (!lik)[]
--- src/types.jl
+++ src/types.jl
@@ -2,22 +2,17 @@ abstract type AbstractThreshold end
 abstract type AbstractLikelihood{T} end
 
 using LogExpFunctions
-using SpecialFunctions
-using StatsFuns
 
-
-@kwdef struct LogisticCDF{T} <: AbstractThreshold
-    μ :: T 
-    σ :: T
-    high :: Bool
+#==================================================================================================
+Likelihood types and manipulation
+==================================================================================================#
+struct Lik{T} <: AbstractLikelihood{T}
+    value :: T 
 end
-
-@kwdef struct GaussianCDF{T} <: AbstractThreshold
-    μ :: T 
-    σ :: T
-    high :: Bool
-end
-
+Lik(x::Lik) = x 
+Lik{T}(x::Lik) where T = Lik{T}(x.value)
+Base.convert(::Type{T}, x::Lik) where T<:Lik = T(x)
+Base.promote_rule(::Type{Lik{T1}},::Type{Lik{T2}}) where {T1,T2} = Lik{promote_type(T1,T2)}
 
 struct LogLik{T} <: AbstractLikelihood{T} 
     value :: T 
@@ -27,3 +22,29 @@ LogLik{T}(x::LogLik) where T = LogLik{T}(x.value)
 Base.convert(::Type{T}, x::LogLik) where T<:LogLik = T(x)
 Base.promote_rule(::Type{LogLik{T1}},::Type{LogLik{T2}}) where {T1,T2} = LogLik{promote_type(T1,T2)}
 
+#Cross conversion between likelihoods and log-likelihoods
+Base.convert(::Type{T}, x) where T<:AbstractLikelihood = T(x)
+(::Type{T})(x::Lik) where T<:LogLik = T(log(x.value))
+(::Type{T})(x::LogLik) where T<:Lik = T(exp(x.value))
+
+#Prefer log-likelihoods when combining due to computational properties 
+Base.promote_rule(::Type{Lik{T1}}, ::Type{LogLik{T2}}) where {T1,T2} = LogLik{promote_type(T1,T2)}
+Base.promote_rule(::Type{LogLik{T1}}, ::Type{Lik{T2}}) where {T1,T2} = LogLik{promote_type(T1,T2)}
+
+
+#==================================================================================================
+Thresholds
+==================================================================================================#
+#Thresholds are callable objects, calling them on a number returns a standardized result
+(θ::AbstractThreshold)(x::Number) = (x-θ.μ)/σ
+
+@kwdef struct Logistic{T} <: AbstractThreshold
+    μ :: T 
+    σ :: T
+end
+
+#Likelihoods
+Lik(θ::Logistic, x::Number) = Lik(logistic(θ(x)))
+Lik{T}(θ::Logistic, x::Number) where T = Lik{T}(logistic(θ(x)))
+LogLik(θ::Logistic, x::Number) = LogLik(loglogistic(θ(x)))
+LogLik{T}(θ::Logistic, x::Number) where T = LogLik(loglogistic(θ(x)))
