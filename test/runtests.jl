69811b6f6e8f63dac7a0b98a3df2635ef56a2095
Ruben Gonzalez <ruben.carbonbased@gmail.com>
2026-06-22 16:04:23 -0600

    Test coverage added

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
 using IndependentProbLogic
 using Test
 
+#============================================================================================================================
+Run these commands at startup to see coverage
+julia --startup-file=no --depwarn=yes --threads=auto -e 'using Coverage; clean_folder("src"); clean_folder("test"); clean_folder("ext") '
+julia --startup-file=no --depwarn=yes --threads=auto --code-coverage=user --project=. -e 'using Pkg; Pkg.test(coverage=true)'
+julia --startup-file=no --depwarn=yes --threads=auto coverage.jl
+
+Run this command for testing invalidations
+julia --startup-file=no --depwarn=yes --threads=auto --project=. test/invalidations.jl
+============================================================================================================================#
+
+
 @testset "IndependentProbLogic.jl" begin
     lik1 = log(Likelihood(0.5))
     lik2 = log(Likelihood(1.0))
